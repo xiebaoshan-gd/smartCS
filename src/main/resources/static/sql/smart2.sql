56d3caeed0dca1f756dfa37869d3af47d74b820e
xiebaoshan_gd <xiebaoshan_gd@163.com>
2019-12-11 19:42:14 +0800

    sql文件

---
+++ src/main/resources/static/sql/smart2.sql
@@ -0,0 +1,220 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.02 
+MySQL - 8.0.16 : Database - smart
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`smart` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `smart`;
+
+/*Table structure for table `asset` */
+
+DROP TABLE IF EXISTS `asset`;
+
+CREATE TABLE `asset` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `num` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `rfid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `asset` */
+
+insert  into `asset`(`id`,`name`,`num`,`rfid`,`state`) values (11,'Air conditioning2','0202','02 35',0),(12,'Air conditioning3','0203','00 86',0),(13,'computer3','0103','83 24',1),(14,'LED1','0301','34 90',0),(15,'LED2','0302','03 83',1),(16,'LED3','0303','87 90',1),(17,'LED4','0304','93 45',0),(18,'computer4','0104','84 35',0),(19,'Air conditioning4','0204','28 49',0),(20,'LED5','0305','47 54',0),(22,'Air conditioning5','0205','43 50',1);
+
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `coursename` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `courseid` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `courseteacher` varchar(20) DEFAULT NULL,
+  `coursecredit` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+
+/*Data for the table `course` */
+
+insert  into `course`(`id`,`coursename`,`courseid`,`courseteacher`,`coursecredit`) values (1,'高数','1001','王','1.5'),(2,'大学英语','1002','徐','2'),(21,'C','1003','孙','2'),(22,'C++','1004','sun','1.5'),(40,'高数','1005','王','2');
+
+/*Table structure for table `cst_customer` */
+
+DROP TABLE IF EXISTS `cst_customer`;
+
+CREATE TABLE `cst_customer` (
+  `cust_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cust_name` varchar(50) DEFAULT NULL,
+  `cust_phone` varchar(50) DEFAULT NULL,
+  `cust_adress` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`cust_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `cst_customer` */
+
+/*Table structure for table `menu` */
+
+DROP TABLE IF EXISTS `menu`;
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '菜单名称',
+  `url` varchar(255) DEFAULT NULL COMMENT '菜单URL',
+  `pid` int(11) NOT NULL DEFAULT '0' COMMENT '父级菜单编号',
+  `remark` varchar(500) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `menu` */
+
+insert  into `menu`(`id`,`name`,`url`,`pid`,`remark`) values (1,'班级','qq.com',0,'111');
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '角色名称',
+  `remark` varchar(500) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`name`,`remark`) values (1,'教师',''),(2,'学生',''),(3,'游客','');
+
+/*Table structure for table `rolemenu` */
+
+DROP TABLE IF EXISTS `rolemenu`;
+
+CREATE TABLE `rolemenu` (
+  `roleId` int(11) NOT NULL,
+  `menuId` int(11) NOT NULL,
+  PRIMARY KEY (`roleId`,`menuId`),
+  KEY `menuId` (`menuId`),
+  CONSTRAINT `rolemenu_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`),
+  CONSTRAINT `rolemenu_ibfk_2` FOREIGN KEY (`menuId`) REFERENCES `menu` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `rolemenu` */
+
+insert  into `rolemenu`(`roleId`,`menuId`) values (1,1);
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `num` varchar(50) DEFAULT NULL,
+  `rfid` varchar(50) DEFAULT NULL,
+  `state` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`,`num`,`rfid`,`state`) values (1,'b','1249','09 08',0),(6,'f','1235','09 05',0),(8,'r','1240','09 04',0),(10,'h','1237','09 02',0),(11,'a','1230','09 09',0),(12,'c','1233','09 07',0),(13,'d','1249','09 06',0),(14,'e','1241','09 02',0),(15,'g','1242','09 02',0),(16,'i','1243','7890',0),(17,'l','1236','1234',0),(18,'q','12','12',0),(19,'w','12345','12 34',0),(21,'1','1','1',0);
+
+/*Table structure for table `tbl_user` */
+
+DROP TABLE IF EXISTS `tbl_user`;
+
+CREATE TABLE `tbl_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) DEFAULT NULL,
+  `user_age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_user` */
+
+insert  into `tbl_user`(`user_id`,`user_name`,`user_age`) values (1,'a',11),(2,'b',12),(3,'c',13),(4,'d',14),(5,'e',15),(6,'f',16),(7,'g',17);
+
+/*Table structure for table `teacher` */
+
+DROP TABLE IF EXISTS `teacher`;
+
+CREATE TABLE `teacher` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `teachername` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '姓名',
+  `teachernum` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '教师编号',
+  `sex` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '性别',
+  `birth` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '出生日期',
+  `starttime` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '入职时间',
+  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '家庭住址',
+  `position` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `teacher` */
+
+insert  into `teacher`(`id`,`teachername`,`teachernum`,`sex`,`birth`,`starttime`,`address`,`position`) values (2,'1','1','2','1','1','1','1'),(3,'王','16001','男','19800101','20100101','广东海洋大学','主任'),(4,'2','2','2','2','2','22','2');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) NOT NULL COMMENT '用户名',
+  `userPass` varchar(255) NOT NULL COMMENT '密码（加密）',
+  `realName` varchar(20) NOT NULL COMMENT '真实姓名',
+  `gender` tinyint(1) NOT NULL DEFAULT '0' COMMENT '性别, 0代表男，1代表女',
+  `phone` varchar(255) DEFAULT NULL COMMENT '联系电话',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userName` (`userName`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`userName`,`userPass`,`realName`,`gender`,`phone`) values (1,'tiger','123','小虎',0,'13522612312');
+
+/*Table structure for table `userrole` */
+
+DROP TABLE IF EXISTS `userrole`;
+
+CREATE TABLE `userrole` (
+  `userId` int(11) NOT NULL,
+  `roleId` int(11) NOT NULL,
+  PRIMARY KEY (`userId`,`roleId`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `userrole_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`),
+  CONSTRAINT `userrole_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `userrole` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `MobName` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '联系人显示的名字',
+  `UserName` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '联系人的名字',
+  `nickname` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '联系人的昵称',
+  `phone` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '联系人的电话',
+  `register` tinyint(4) NOT NULL COMMENT '是1否0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`MobName`,`UserName`,`nickname`,`phone`,`register`) values (1,'小三','张三','三哥','13123232323',1),(2,'四弟','李四','阿肆','15356739929',1),(3,'五天','王五','小五','15555555555',1),(4,'玉圆环','杨玉洁','小玉','13145357896',1),(5,'余力','于大力','大力','15467889192',0),(6,'和虎','陈虎','阿虎','15545656655',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
